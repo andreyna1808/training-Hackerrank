8a9991462467fba33a6083728282ffc511fe89ef
andreyna1808 <andreyna.m.carvalho@gmail.com>
2023-01-27 06:50:42 -0400

    Crash Override CodeWars

---
+++ kyu8/11_CreashOverride.js
@@ -0,0 +1,19 @@
+// https://www.codewars.com/kata/578c1e2edaa01a9a02000b7f/train/javascript
+
+function aliasGen(name, lastName) {
+  const existName = (value) =>
+    value
+      .split("")
+      .shift()
+      .replace(/[^A-z]/g, "")
+      .toUpperCase();
+
+  const isName = existName(name);
+  const isSurName = existName(lastName);
+
+  if (!isName.length > 0 || !isSurName.length > 0) {
+    return "Your name must start with a letter from A - Z.";
+  }
+
+  return `${firstName[isName]} ${surname[isSurName]}`;
+}
